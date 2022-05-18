140340a7a470bfdd6f7e2372c6d4648c1fa40ce4
Sherreka <rekadarnb@gmail.com>
2022-05-18 17:29:35 -0400

    added DOM elements

---
+++ index.js
@@ -0,0 +1,8 @@
+//DOM elements
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
