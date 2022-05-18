0f1dbcd98908b70436bb1c291789d432ad284bb4
Sherreka <rekadarnb@gmail.com>
2022-05-18 18:42:08 -0400

    added more functions

--- index.js
+++ index.js
@@ -23,14 +23,40 @@ class Calculator {
     }
 
     chooseOperation(operation){
+        if(this.currentOperand === '') return
+        if(this.previousOperand !== ''){
+            this.compute()
+        }
         this.operation = operation;
         this.previousOperand = this.currentOperand;
-        this.currentOperand = ''
+        this.currentOperand = '';
         
     }
 
     compute(){
-
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation){
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
     }
 
     updateDisplay(){
@@ -66,4 +92,9 @@ operationButtons.forEach(button => {
         calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    updateDisplay()
 })
